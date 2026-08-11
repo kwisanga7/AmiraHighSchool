0203cbe1a961163535a31a2a74c454e5be140025
Elie Kwisanga <eliekwisanga95@gmail.com>
2026-08-11 15:16:52 +0200

    Implement student reistration system

--- app.py
+++ app.py
@@ -1,10 +1,44 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from database import register_student
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Welcome to Amira High School Management System"
+    return """
+    <h1>Welcome to Amira High School</h1>
+    <a href='/register'>Register</a>
+    """
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+
+    if request.method == "POST":
+
+        full_name = request.form["full_name"]
+        age = request.form["age"]
+        sector = request.form["sector"]
+        date_of_birth = request.form["date_of_birth"]
+        phone = request.form["phone"]
+        password = request.form["password"]
+
+        student_id = register_student(
+            full_name,
+            age,
+            sector,
+            date_of_birth,
+            phone,
+            password
+        )
+
+        return f"""
+        Registration Successful!<br><br>
+        Your Student ID is: <b>{student_id}</b><br><br>
+        <a href='/register'>Register Another Student</a>
+        """
+
+    return render_template("register.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
